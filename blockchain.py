e2431cb85a6553d677cd415f0f4516a6b5571195
Pargorn Ruasijan <pargornruasijan@gmail.com>
2021-11-27 20:24:47 +0700

    Add function create_block

--- blockchain.py
+++ blockchain.py
@@ -9,3 +9,13 @@ class Blockchain:
         self.total_bill = 0
         self.chain = []
         self.create_block(nonce = 1, previous_hash = '0')
+
+    def create_block(self, nonce, previous_hash):
+        block = {'index': len(self.chain) + 1,
+        'total_bill': self.total_bill,
+        'timestamp': str(datetime.datetime.now()),
+        'nonce': nonce,
+        'previous_hash': previous_hash,
+        }
+        self.chain.append(block) # นำไปต่อกับ block เดิมที่สร้างก่อนหน้า
+        return block
