e56ba699bf9b0d0df6c105c16495bc9dc41abe3e
Pargorn Ruasijan <pargornruasijan@gmail.com>
2021-11-27 20:29:23 +0700

    Create mine block

--- blockchain.py
+++ blockchain.py
@@ -65,3 +65,22 @@ app = Flask(__name__)
 
 #Create Blockchain
 blockchain = Blockchain()
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_nonce = previous_block['nonce']
+    nonce = blockchain.nonce_of_work(previous_nonce)
+
+    previous_hash = blockchain.hash(previous_block)
+
+    block = blockchain.create_block(nonce, previous_hash)
+
+    response = {
+        'message': 'Congratulations, you just mined a block',
+        'index': block['index'],
+        'timestamp': block['timestamp'],
+        'nonce': block['nonce'],
+        'previous_hash': block['previous_hash']
+    }
+    return jsonify(response), 200
