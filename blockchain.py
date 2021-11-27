87bdbb8ac9e218a3f3e7a7c8070a377f708cb6c8
Pargorn Ruasijan <pargornruasijan@gmail.com>
2021-11-27 20:24:02 +0700

    Add class Blockchain

--- blockchain.py
+++ blockchain.py
@@ -2,3 +2,10 @@ import datetime
 import hashlib
 import json
 from flask import Flask, jsonify
+
+class Blockchain:
+
+    def __init__(self):
+        self.total_bill = 0
+        self.chain = []
+        self.create_block(nonce = 1, previous_hash = '0')
