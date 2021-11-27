af792107c08b2d69a0e1ad6a0f217b10fe53fc91
Pargorn Ruasijan <pargornruasijan@gmail.com>
2021-11-27 20:25:54 +0700

    Add function nonce_of_work

--- blockchain.py
+++ blockchain.py
@@ -22,3 +22,16 @@ class Blockchain:
 
     def get_previous_block(self):
         return self.chain[-1]
+
+    def nonce_of_work(self, previous_nonce):
+        new_nonce = 1
+        check_nonce = False
+
+        while check_nonce is False:
+            hash_operation = hashlib.sha256(str(new_nonce**2 - previous_nonce**1).encode()).hexdigest()
+            #target
+            if hash_operation[:4] == '0000':
+                check_nonce = True
+            else:
+                new_nonce += 1
+        return new_nonce
